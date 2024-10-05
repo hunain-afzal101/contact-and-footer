0664d1967197539fec88ca31d928389f38f6357e
hunain-afzal101 <156126292+hunain-afzal101@users.noreply.github.com>
2024-10-05 13:14:39 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Contact from './Contact'
+import Footer from './Footer'
+
+function App() {
+ 
+
+  return (
+    <>
+   <Contact/>
+   <Footer/>
+    </>
+  )
+}
+
+export default App
